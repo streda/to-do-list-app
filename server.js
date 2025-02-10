76cb3a2da0228c514dd0efc4402fdc57f6170266
streda <samuelreda2018@gmail.com>
2025-02-10 01:06:54 -0800

    First EC2 Launch Instance

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }, // Add this line
 });
 
 async function initializeDatabase() {
